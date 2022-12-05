4194cd0133fc2082fa494caf94aa76643fcaf5e5
Jack <Jwenschhof@bsd220.org>
2022-12-05 09:58:10 -0600

    Initial Commit

---
+++ Calorie Counting App/Calorie_Counting_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calorie_Counting_AppApp.swift
+//  Calorie Counting App
+//
+//  Created by Jack W on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Calorie_Counting_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
