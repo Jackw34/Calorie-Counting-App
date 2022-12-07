b0e24cebab337582448c5445915bba4499001bb3
Jack <Jwenschhof@bsd220.org>
2022-12-07 09:39:32 -0600

    added a tittle as well as UI to the App

--- Calorie Counting App/ContentView.swift
+++ Calorie Counting App/ContentView.swift
@@ -8,16 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var managedObjContext
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.date, order: .reverse)]) var food: FetchedResults<Food>
+    
+    @State private var showingAddView = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            // view to the instructions
+            VStack(alignment: .leading) {
+                NavigationLink(destination: Instructions()) {
+                    Text("Instructions For The App")
+                        .font(.system(size: 25))
+                }
+                // where the total calories are counted and displayed
+                Text("\(Int(totalCaloriesToday())) Cal")
+                    .foregroundColor(.gray)
+                    .padding([.horizontal])
+                
+                List {
+                    // The edit view
+                    ForEach(food) { food in
+                        NavigationLink(destination: EditFoodView(food: food)) {
+                            HStack {
+                                VStack(alignment: .leading, spacing: 6) {
+                                    Text(food.name!)
+                                        .bold()
+        
+                                    Text("\(Int(food.calories))") + Text(" calories").foregroundColor(.red)
+                                }
+                                Spacer()
+                            }
+                        }
+                    }
+                    .onDelete(perform: deleteFood) // allows you to delete what you added
+                }
+                .listStyle(.plain)
+            }
+            // The title
+            .navigationTitle("Calorie Counter")
+            // adds the image to where you add food
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        showingAddView.toggle()
+                    } label: {
+                        Label("Add food", systemImage: "plus.circle")
+                    }
+                }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+            }
+            .sheet(isPresented: $showingAddView) {
+                AddFoodView()
+            }
         }
-        .padding()
+        .navigationViewStyle(.stack) // Removes sidebar on iPad
+    }
+    
+    // The function that deletes food that has been added
+    private func deleteFood(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { food[$0] }
+            .forEach(managedObjContext.delete)
+            
+            // Saves to our database
+            DataController().save(context: managedObjContext)
+        }
+    }
+    
+    // Calculates the total calories consumed today
+    private func totalCaloriesToday() -> Double {
+        var caloriesToday : Double = 0
+        for item in food {
+            if Calendar.current.isDateInToday(item.date!) {
+                caloriesToday += item.calories
+            }
+        }
+        print("Calories today: \(caloriesToday)")
+        return caloriesToday
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
